124f540c46cc0d4ef352f9a981c9f8ebd7a606b5
Tim Yoder <tim.a.yoder@gmail.com>
2020-12-03 17:26:34 -0800

    allow debugging testing

--- covid_tracker/heroku.py
+++ covid_tracker/heroku.py
@@ -8,8 +8,8 @@ DATABASES = {
     )
 }
 
-DEBUG = False
-TEMPLATE_DEBUG = False
+# DEBUG = False
+# TEMPLATE_DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = ["*"]
